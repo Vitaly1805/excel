6346a0bdf17e4b3c36c3d942786e7cc75fc5a65b
Nosov Vitaly <vitalinosov2000@gmail.com>
2023-01-09 18:33:03 +0300

    Decomposition of logic of resize table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,12 +1,11 @@
 import {ExcelComponent} from "@core/ExcelComponent";
-import {setStyles} from "../../core/utils";
+import {Resize} from "./table.resize";
+import {shouldResize} from './table.functions'
 import {createTable} from "./table.template";
 
 export class Table extends ExcelComponent {
   static className = 'table-excel'
   countRow = 25
-  minCellWidth = 40
-  minCellHeight = 25
 
   constructor($root) {
     super($root, {
@@ -23,83 +22,8 @@ export class Table extends ExcelComponent {
   }
 
   onMousedown(event) {
-    const $resizer = event.target
-    const dataset = $resizer.dataset
-
-    if (dataset.resize) {
-      const $resizableElem = $resizer.closest('[data-type="resizable"]')
-      const typeResize = dataset.resize
-      $resizer.classList.add('_active')
-
-      document.onmousemove = e => this.onMousemove.apply(this, [e, $resizer, $resizableElem, typeResize])
-      document.onmouseup = () => this.onMouseup.apply(this, [$resizer, $resizableElem, typeResize])
-    }
-  }
-
-  onMousemove(e, $resizer, $resizableElem, typeResize) {
-    const coords = $resizableElem.getBoundingClientRect()
-
-    if (typeResize === 'col') {
-      const width = e.clientX - coords.x
-      this.setColWidth(width)
-    } else if (typeResize === 'row') {
-      const height = e.clientY - $resizableElem.offsetTop
-      this.setRowHeight(height)
-    }
-
-    this.setResizeCoords($resizer, e, coords, typeResize)
-  }
-
-  onMouseup($resizer, $resizableElem, typeResize) {
-    $resizer.classList.remove('_active')
-    document.onmousemove = null
-    document.onmouseup = null
-
-    if (typeResize === 'col') {
-      this.addWidthCells($resizableElem)
-      setStyles($resizer, {'right': '0px'})
-    } else {
-      setStyles($resizableElem, {'height': `${this.height}px`})
-      setStyles($resizer, {'bottom': '0px'})
-    }
-  }
-
-  setColWidth(width) {
-    if (width >= this.minCellWidth) {
-      this.width = width
-    } else {
-      this.width = this.minCellWidth
-    }
-  }
-
-  setRowHeight(height) {
-    if (height >= this.minCellHeight) {
-      this.height = height
-    } else {
-      this.height = this.minCellHeight
-    }
-  }
-
-  addWidthCells($resizableElem) {
-    const letter = $resizableElem.dataset.col
-    const $cells = this.$root.querySelectorAll(`[data-col="${letter}"]`)
-
-    $cells.forEach($elem => {
-      setStyles($elem, {'width': `${this.width}px`})
-    })
-  }
-
-  setResizeCoords($resizer, event, coords, typeResize) {
-    if (typeResize === 'col') {
-      if (this.width > this.minCellWidth) {
-        const resizePadding = coords.right - event.clientX
-        setStyles($resizer, {'right': `${resizePadding}px`})
-      }
-    } else {
-      if (this.height > this.minCellHeight) {
-        const resizePadding = coords.bottom - event.clientY
-        setStyles($resizer, {'bottom': `${resizePadding}px`})
-      }
+    if (shouldResize(event)) {
+      Resize.start(event, this.$root)
     }
   }
 }
--- src/components/table/table.resize.js
+++ src/components/table/table.resize.js
@@ -1,87 +1,88 @@
-// import {setStyles} from '@core/utils.js'
+import {setStyles} from '@core/utils.js'
 
-// const minCellWidth = 40
-// const minCellHeight = 25
-// let currentWidth
-// let currentHeight
-// let $root
+export class Resize {
+  static minCellWidth = 40
+  static minCellHeight = 25
 
-// export function resizeHandler(event, $elem) {
-//   $root = $elem
-//   const $resizer = event.target
-//   const dataset = $resizer.dataset
-//   const $resizableElem = $resizer.closest('[data-type="resizable"]')
-//   const typeResize = dataset.resize
+  static start(event, $root) {
+    this.$root = $root
+    const $resizer = event.target
+    const dataset = $resizer.dataset
 
-//   $resizer.classList.add('_active')
+    if (dataset.resize) {
+      const $resizableElem = $resizer.closest('[data-type="resizable"]')
+      const typeResize = dataset.resize
+      $resizer.classList.add('_active')
 
-//   document.onmousemove = e => onMousemove.apply({}, [e, $resizer, $resizableElem, typeResize])
-//   document.onmouseup = () => onMouseup.apply({}, [$resizer, $resizableElem, typeResize])
-// }
+      document.onmousemove = e => this.onMousemove.apply(this, [e, $resizer, $resizableElem, typeResize])
+      document.onmouseup = () => this.onMouseup.apply(this, [$resizer, $resizableElem, typeResize])
+    }
+  }
 
-// function onMousemove(e, $resizer, $resizableElem, typeResize) {
-//   const coords = $resizableElem.getBoundingClientRect()
+  static onMousemove(e, $resizer, $resizableElem, typeResize) {
+    const coords = $resizableElem.getBoundingClientRect()
 
-//   if (typeResize === 'col') {
-//     const width = e.clientX - coords.x
-//     setColWidth(width)
-//   } else if (typeResize === 'row') {
-//     const height = e.clientY - $resizableElem.offsetTop
-//     setRowHeight(height)
-//   }
+    if (typeResize === 'col') {
+      const width = e.clientX - coords.x
+      this.setColWidth(width)
+    } else if (typeResize === 'row') {
+      const height = e.clientY - $resizableElem.offsetTop
+      this.setRowHeight(height)
+    }
 
-//   setResizeCoords($resizer, e, coords, typeResize)
-// }
+    this.setResizeCoords($resizer, e, coords, typeResize)
+  }
 
-// function onMouseup($resizer, $resizableElem, typeResize) {
-//   $resizer.classList.remove('_active')
-//   document.onmousemove = null
-//   document.onmouseup = null
+  static onMouseup($resizer, $resizableElem, typeResize) {
+    $resizer.classList.remove('_active')
+    document.onmousemove = null
+    document.onmouseup = null
 
-//   if (typeResize === 'col') {
-//     addWidthCells($resizableElem)
-//     setStyles($resizer, {'right': '0px'})
-//   } else {
-//     setStyles($resizableElem, {'height': `${currentHeight}px`})
-//     setStyles($resizer, {'bottom': '0px'})
-//   }
-// }
+    if (typeResize === 'col') {
+      this.addWidthCells($resizableElem)
+      setStyles($resizer, {'right': '0px'})
+    } else {
+      setStyles($resizableElem, {'height': `${this.height}px`})
+      setStyles($resizer, {'bottom': '0px'})
+    }
+  }
 
-// function setColWidth(width) {
-//   if (width >= minCellWidth) {
-//     currentWidth = width
-//   } else {
-//     currentWidth = minCellWidth
-//   }
-// }
+  static setColWidth(width) {
+    if (width >= this.minCellWidth) {
+      this.width = width
+    } else {
+      this.width = this.minCellWidth
+    }
+  }
 
-// function setRowHeight(height) {
-//   if (height >= minCellHeight) {
-//     currentHeight = height
-//   } else {
-//     currentHeight = minCellHeight
-//   }
-// }
+  static setRowHeight(height) {
+    if (height >= this.minCellHeight) {
+      this.height = height
+    } else {
+      this.height = this.minCellHeight
+    }
+  }
 
-// function addWidthCells($resizableElem) {
-//   const letter = $resizableElem.dataset.col
-//   const $cells = $root.querySelectorAll(`[data-col="${letter}"]`)
+  static addWidthCells($resizableElem) {
+    const letter = $resizableElem.dataset.col
+    const $cells = this.$root.querySelectorAll(`[data-col="${letter}"]`)
 
-//   $cells.forEach($elem => {
-//     setStyles($elem, {'width': `${currentWidth}}px`})
-//   })
-// }
+    $cells.forEach($elem => {
+      setStyles($elem, {'width': `${this.width}px`})
+    })
+  }
 
-// function setResizeCoords($resizer, event, coords, typeResize) {
-//   if (typeResize === 'col') {
-//     if (currentWidth > minCellWidth) {
-//       const resizePadding = coords.right - event.clientX
-//       setStyles($resizer, {'right': `${resizePadding}px`})
-//     }
-//   } else {
-//     if (currentHeight > minCellHeight) {
-//       const resizePadding = coords.bottom - event.clientY
-//       setStyles($resizer, {'bottom': `${resizePadding}px`})
-//     }
-//   }
-// }
+  static setResizeCoords($resizer, event, coords, typeResize) {
+    if (typeResize === 'col') {
+      if (this.width > this.minCellWidth) {
+        const resizePadding = coords.right - event.clientX
+        setStyles($resizer, {'right': `${resizePadding}px`})
+      }
+    } else {
+      if (this.height > this.minCellHeight) {
+        const resizePadding = coords.bottom - event.clientY
+        setStyles($resizer, {'bottom': `${resizePadding}px`})
+      }
+    }
+  }
+}
